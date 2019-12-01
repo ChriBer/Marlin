09889dc5c5bee53099c21e9bd80eb7369791d8d9
Christian Berger <berger-christian@web.de>
2019-12-01 20:49:06 +0100

    x/y directions inverted

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2163,7 +2163,7 @@
   #define CUSTOM_USER_MENU_TITLE "Custom Commands"
   #define USER_SCRIPT_DONE "M117 User Script Done"
   #define USER_SCRIPT_AUDIBLE_FEEDBACK
-  //#define USER_SCRIPT_RETURN  // Return to status screen after a script
+  #define USER_SCRIPT_RETURN  // Return to status screen after a script
 
   #define USER_DESC_1 "Reset All Coordinates"
   #define USER_GCODE_1 "G92 X0 Y0 Z0"
